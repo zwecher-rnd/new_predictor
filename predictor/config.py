f8cdd392b99ad61998b91e5b7ea287fc7a565e97
elchanan <elchanan4567@gmail.com>
2019-12-17 10:05:28 +0200

    video and models

--- predictor/config.py
+++ predictor/config.py
@@ -1,14 +1,14 @@
 BLUR = False
-LABELS_PATH = r"D:\predictor_data\data\buildings"  # set_this
-OBS_PATH = r"D:\predictor_data\data\observations"  # set_this
+LABELS_PATH = r"D:\predictor_data\data\buildings_global"  # set_this
+OBS_PATH = r"D:\predictor_data\data\observations_global"  # set_this
 PADDING = 16
-HEIGHT = 62
-WIDTH = 60
+HEIGHT = 80
+WIDTH = 80
 DENSE = True
 NUM_OF_OBS = 5
 KERNEL_SIZE = 3
 VAR = 1
-OUTPUT_PATH_TOKEN = "6260"
+OUTPUT_PATH_TOKEN = "8080"
 MODELS_PATH = r"C:\Users\zwecher\Documents\mygit\Indoors_Capabilities\models_predictor\models_" + OUTPUT_PATH_TOKEN
 
 BUILDING_NUM = "building_num"
--- predictor/full_model_predictor.py
+++ predictor/full_model_predictor.py
@@ -25,7 +25,7 @@ def get_dense_model(conv_layers_num=9, kernel_size=3):
     inp = obs
     for l in range(conv_layers_num):
         conv = Conv2D(filters=25, kernel_size=kernel_size, activation="relu", name=f"conv_{l}")(inp)
-        if l == conv_layers_num//2:
+        if l == conv_layers_num // 2:
             inp = MaxPool2D(pool_size=2, strides=2, name=f"max_pool_2d_{l}")(conv)
         else:
             inp = conv
@@ -58,7 +58,7 @@ def get_conv_model(conv_layers_num=9, kernel_size=3):
         inp = Conv2DTranspose(filters=25, kernel_size=kernel_size, strides=2, padding="same",
                               name="trnasp_{}".format(l))(inp)
     last_conv = Conv2D(filters=1, kernel_size=3, padding="same", name="last_conv")(inp)
-    logits = Flatten(name = "flat")(last_conv)
+    logits = Flatten(name="flat")(last_conv)
     loss_for_logits = utils.sigmoid_cross_entropy_with_logits_layer(name="loss_for_logits")([labels, logits])
     mask = utils.get_mask_layer(name="mask")(obs)
     masked_loss = Multiply(name="masked_loss")([loss_for_logits, mask])
@@ -104,7 +104,8 @@ def train_model(model, epochs=5, batch_size=64):
     dg = batch_manager.BuildingsGenrator(x_set=train_metadata_df.iloc[:, 0], y_set=train_metadata_df.iloc[:, 1],
                                          batch_size=batch_size)
     validation_data = batch_manager.get_samples(test_metadata_df)
-    filepath = r"C:\Users\zwecher\Documents\mygit\new_predictor\models\models_6260_dense\model-{epoch:02d}-{loss:.4f}.hdf5"
+    filepath = r"C:\Users\zwecher\Documents\mygit\new_predictor\models\models_" + config.OUTPUT_PATH_TOKEN + \
+               r"_conv\model-{epoch:02d}-{loss:.4f}.hdf5"
     checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
     callbacks_list = [checkpoint]
     model.fit_generator(generator=dg, epochs=epochs, verbose=1, validation_data=validation_data,
